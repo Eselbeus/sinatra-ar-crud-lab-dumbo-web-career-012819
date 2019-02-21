b32db62896200e1b2d6033004efc0cf5897704e1
Seann Branchfield <eselbeus@gmail.com>
2019-02-21 00:19:45 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,56 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :method_override, true
   end
 
   get '/' do
+    @articles = Article.all
+    # binding.pry
+    erb :index
+  end
+  
+  get '/articles/new' do
+    erb :new
+  end
+  
+  get '/articles' do
+    @articles = Article.all
+    # binding.pry
+    erb :index
+  end
+  
+  post '/articles' do 
+    new_article = Article.create(params)
+    # binding.pry
+    redirect to "/articles/#{new_article.id.to_s}"
+  end
+  
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    # binding.pry
+    erb :show
+  end
+  
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    # binding.pry
+    erb :edit
+  end
+  
+  # post '/articles/:id/edit' do 
+  #   erb :edit
+  # end
+  
+  patch '/articles/:id' do 
+    @article = Article.find(params[:id])
+    @article.update(params)
+    # @article.save
+    # binding.pry
+    redirect to "/articles/#{@article.id.to_s}"
+  end
+  
+  delete '/articles/:id' do
+    erb :index
   end
 end
